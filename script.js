f6665aa1a2907b63c1f743a81faf7939260947ad
Jcireland14 <Jcireland14@gmail.com>
2017-03-08 16:51:07 -0500

    working on questions

---
+++ script.js
@@ -0,0 +1,228 @@
+
+////////////ONE HUNDRED QUESTIONS ///////////////////////////
+//SPORTS MOVIES MUSIC HISTORY MISC
+////////SPORTS
+
+$("#button-100A").click( function()
+           {
+var questionOneA = prompt("What was the name of Atlanta's NHL team?\n\n\nA) Flyers B) Panthers C) Hawks D) Thrashers");
+   if (questionOneA === "D"){
+       alert("That is correct! Please add 100 to your score");
+       }
+       else {
+        alert("That is incorrect. Please subtract 100 from your score");
+         }
+           }
+      );
+// /////////MOVIES
+$("#button-100B").click( function()
+          {
+var questionOneB = prompt("The snozzberries taste like snozzberries.\n\n\nA) Willy Wonka B) Super Troopers C) Alice in Wonderland D) Avengers");
+   if (questionOneB === "B"){
+       alert("That is correct! Please add 100 to your score");
+       }
+       else {
+        alert("That is incorrect. Please subtract 100 from your score");
+         }
+           }
+      );
+// /////////MUSIC
+$("#button-100C").click( function()
+          {
+var questionOneC = prompt("Rockstar lifestyle, might dont make it.\n\n\nA) Ridin' dirty B) Lemonade C) Wasted D) Wheels on the bus");
+   if (questionOneC === "C"){
+       alert("That is correct! Please add 100 to your score");
+       }
+       else {
+        alert("That is incorrect. Please subtract 100 from your score");
+         }
+           }
+      );
+// //////HISTORY
+// var questionOneD = prompt("What was the name of Atlanta's NHL team?\n\n\nA) Flyers B) Panthers C) Hawks D) Thrashers");
+// if (questionOneD === "D") {
+//    alert("That Is Correct!");
+// }
+//   else {
+//    alert("Sorry that is incorrect");
+//    }
+// ///////MISC
+// var questionOneE = prompt("What was the name of Atlanta's NHL team?\n\n\nA) Flyers B) Panthers C) Hawks D) Thrashers");
+// if (questionOneE === "E") {
+//    alert("That Is Correct!");
+// }
+//   else {
+//    alert("Sorry that is incorrect");
+//    }
+
+// ////////////TWO HUNDRED QUESTIONS ///////////////////////////
+// //SPORTS MOVIES MUSIC HISTORY MISC
+// ////////SPORTS
+// var questionTwoA = prompt("What was the name of Atlanta's NHL team?\n\n\nA) Flyers B) Panthers C) Hawks D) Thrashers");
+// if (questionTwoA === "D") {
+//    alert("That Is Correct!");
+// }
+//   else {
+//    alert("Sorry that is incorrect");
+//    }
+// /////////MOVIES
+// var questionTwoB = prompt("What was the name of Atlanta's NHL team?\n\n\nA) Flyers B) Panthers C) Hawks D) Thrashers");
+// if (questionTwoB === "B") {
+//    alert("That Is Correct!");
+// }
+//   else {
+//    alert("Sorry that is incorrect");
+//    }
+// /////////MUSIC
+// var questionTwoC = prompt("What was the name of Atlanta's NHL team?\n\n\nA) Flyers B) Panthers C) Hawks D) Thrashers");
+// if (questionTwoC=== "C") {
+//    alert("That Is Correct!");
+// }
+//   else {
+//    alert("Sorry that is incorrect");
+//    }
+// //////HISTORY
+// var questionTwoD = prompt("What was the name of Atlanta's NHL team?\n\n\nA) Flyers B) Panthers C) Hawks D) Thrashers");
+// if (questionTwoD === "D") {
+//    alert("That Is Correct!");
+// }
+//   else {
+//    alert("Sorry that is incorrect");
+//    }
+// ///////MISC
+// var questionTwoE = prompt("What was the name of Atlanta's NHL team?\n\n\nA) Flyers B) Panthers C) Hawks D) Thrashers");
+// if (questionTwoE === "E") {
+//    alert("That Is Correct!");
+// }
+//   else {
+//    alert("Sorry that is incorrect");
+//    }
+
+// ////////////THREE HUNDRED QUESTIONS ///////////////////////////
+// //SPORTS MOVIES MUSIC HISTORY MISC
+// ////////SPORTS
+// var questionThreeA = prompt("What was the name of Atlanta's NHL team?\n\n\nA) Flyers B) Panthers C) Hawks D) Thrashers");
+// if (questionThreeA === "D") {
+//    alert("That Is Correct!");
+// }
+//   else {
+//    alert("Sorry that is incorrect");
+//    }
+// /////////MOVIES
+// var questionThreeB = prompt("What was the name of Atlanta's NHL team?\n\n\nA) Flyers B) Panthers C) Hawks D) Thrashers");
+// if (questionThreeB === "B") {
+//    alert("That Is Correct!");
+// }
+//   else {
+//    alert("Sorry that is incorrect");
+//    }
+// /////////MUSIC
+// var questionThreeC = prompt("What was the name of Atlanta's NHL team?\n\n\nA) Flyers B) Panthers C) Hawks D) Thrashers");
+// if (questionThreeC=== "C") {
+//    alert("That Is Correct!");
+// }
+//   else {
+//    alert("Sorry that is incorrect");
+//    }
+// //////HISTORY
+// var questionThreeD = prompt("What was the name of Atlanta's NHL team?\n\n\nA) Flyers B) Panthers C) Hawks D) Thrashers");
+// if (questionThreeD === "D") {
+//    alert("That Is Correct!");
+// }
+//   else {
+//    alert("Sorry that is incorrect");
+//    }
+// ///////MISC
+// var questionThreeE = prompt("What was the name of Atlanta's NHL team?\n\n\nA) Flyers B) Panthers C) Hawks D) Thrashers");
+// if (questionThreeE === "E") {
+//    alert("That Is Correct!");
+// }
+//   else {
+//    alert("Sorry that is incorrect");
+//    }
+
+// ////////////FOUR HUNDRED QUESTIONS ///////////////////////////
+// //SPORTS MOVIES MUSIC HISTORY MISC
+// ////////SPORTS
+// var questionFourA = prompt("What was the name of Atlanta's NHL team?\n\n\nA) Flyers B) Panthers C) Hawks D) Thrashers");
+// if (questionFourA === "D") {
+//    alert("That Is Correct!");
+// }Four
+//   else {
+//    alert("Sorry that is incorrect");
+//    }
+// /////////MOVIES
+// var questionFourB = prompt("What was the name of Atlanta's NHL team?\n\n\nA) Flyers B) Panthers C) Hawks D) Thrashers");
+// if (questionFourB === "B") {
+//    alert("That Is Correct!");
+// }
+//   else {
+//    alert("Sorry that is incorrect");
+//    }
+// /////////MUSIC
+// var questionFourC = prompt("What was the name of Atlanta's NHL team?\n\n\nA) Flyers B) Panthers C) Hawks D) Thrashers");
+// if (questionFourC=== "C") {
+//    alert("That Is Correct!");
+// }
+//   else {
+//    alert("Sorry that is incorrect");
+//    }
+// //////HISTORY
+// var questionFourD = prompt("What was the name of Atlanta's NHL team?\n\n\nA) Flyers B) Panthers C) Hawks D) Thrashers");
+// if (questionFourD === "D") {
+//    alert("That Is Correct!");
+// }
+//   else {
+//    alert("Sorry that is incorrect");
+//    }
+// ///////MISC
+// var questionFourE = prompt("What was the name of Atlanta's NHL team?\n\n\nA) Flyers B) Panthers C) Hawks D) Thrashers");
+// if (questionFourE === "E") {
+//    alert("That Is Correct!");
+// }
+//   else {
+//    alert("Sorry that is incorrect");
+//    }
+
+// ////////////FIVE HUNDRED QUESTIONS ///////////////////////////
+// //SPORTS MOVIES MUSIC HISTORY MISC
+// ////////SPORTS
+// var questionFiveA = prompt("What was the name of Atlanta's NHL team?\n\n\nA) Flyers B) Panthers C) Hawks D) Thrashers");
+// if (questionFiveA === "D") {
+//    alert("That Is Correct!");
+// }
+//   else {
+//    alert("Sorry that is incorrect");
+//    }
+// /////////MOVIES
+// var questionFiveB = prompt("What was the name of Atlanta's NHL team?\n\n\nA) Flyers B) Panthers C) Hawks D) Thrashers");
+// if (questionFiveB === "B") {
+//    alert("That Is Correct!");
+// }
+//   else {
+//    alert("Sorry that is incorrect");
+//    }
+// /////////MUSIC
+// var questionFiveC = prompt("What was the name of Atlanta's NHL team?\n\n\nA) Flyers B) Panthers C) Hawks D) Thrashers");
+// if (questionFiveC=== "C") {
+//    alert("That Is Correct!");
+// }
+//   else {
+//    alert("Sorry that is incorrect");
+//    }
+// //////HISTORY
+// var questionFiveD = prompt("What was the name of Atlanta's NHL team?\n\n\nA) Flyers B) Panthers C) Hawks D) Thrashers");
+// if (questionFiveD === "D") {
+//    alert("That Is Correct!");
+// }
+//   else {
+//    alert("Sorry that is incorrect");
+//    }
+// ///////MISC
+// var questionFiveE = prompt("What was the name of Atlanta's NHL team?\n\n\nA) Flyers B) Panthers C) Hawks D) Thrashers");
+// if (questionFiveE === "E") {
+//    alert("That Is Correct!");
+// }
+//   else {
+//    alert("Sorry that is incorrect");
+//    }
