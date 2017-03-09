d73972f2b78e337807a3b1ee064362e71ad0f74e
Jcireland14 <Jcireland14@gmail.com>
2017-03-08 20:46:21 -0500

    more questions down

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 ////////////ONE HUNDRED QUESTIONS ///////////////////////////
-//SPORTS MOVIES MUSIC HISTORY MISC
+//SPORTS MOVIES MUSIC HISTORY SCIENCE
 ////////SPORTS
 
 $("#button-100A").click( function()
@@ -50,7 +50,7 @@ var questionOneD = prompt("This was the last US state to gain its statehood\n\nA
          }
            }
       );
-/////////MISC
+//////// SCIENCE
 $("#button-100E").click( function()
           {
 var questionOneE = prompt("This is the study of living organisms\n\nA) Organology \n\nB) Sociology \n\nC) Sneakerology \n\nD) Biology");
@@ -64,16 +64,16 @@ var questionOneE = prompt("This is the study of living organisms\n\nA) Organolog
       );
 
 // ////////////TWO HUNDRED QUESTIONS ///////////////////////////
-// //SPORTS MOVIES MUSIC HISTORY MISC
+// //SPORTS MOVIES MUSIC HISTORY SCIENCE
 // ////////SPORTS
 $("#button-200A").click( function()
           {
 var questionTwoA = prompt("This sport involves ice, brooms, and a rock\n\nA) Rockball \n\nB) Curling \n\nC) Hockey \n\nD) Figure Skating");
    if (questionTwoA === "B"){
-       alert("That is correct! Please add 100 to your score");
+       alert("That is correct! Please add 200 to your score");
        }
        else {
-        alert("That is incorrect. Please subtract 100 from your score");
+        alert("That is incorrect. Please subtract 200 from your score");
          }
            }
       );
@@ -82,10 +82,10 @@ $("#button-200B").click( function()
           {
 var questionTwoB = prompt("I'm making waffles!\n\nA) Wafflehouse \n\nB) Sharktales \n\nC) Shrek \n\nD) Finding Nemo");
    if (questionTwoB === "C"){
-       alert("That is correct! Please add 100 to your score");
+       alert("That is correct! Please add 200 to your score");
        }
        else {
-        alert("That is incorrect. Please subtract 100 from your score");
+        alert("That is incorrect. Please subtract 200 from your score");
          }
            }
       );
@@ -94,10 +94,10 @@ $("#button-200C").click( function()
           {
 var questionTwoC = prompt("I'm goin down down baby, yo' street in a Range Rover\n\nA) Hot In Herr \n\nB) Country Grammar \n\nC) Ride Wit Me \n\nD) In Da Club");
    if (questionTwoC === "B"){
-       alert("That is correct! Please add 100 to your score");
+       alert("That is correct! Please add 200 to your score");
        }
        else {
-        alert("That is incorrect. Please subtract 100 from your score");
+        alert("That is incorrect. Please subtract 200 from your score");
          }
            }
       );
@@ -106,100 +106,140 @@ $("#button-200D").click( function()
           {
 var questionTwoD = prompt("Abraham Lincoln was this number president of the United States\n\nA) 1st\n\nB) 22nd \n\nC) 31st \n\nD) 16th");
    if (questionTwoD === "D"){
-       alert("That is correct! Please add 100 to your score");
+       alert("That is correct! Please add 200 to your score");
        }
        else {
-        alert("That is incorrect. Please subtract 100 from your score");
+        alert("That is incorrect. Please subtract 200 from your score");
          }
            }
       );
-// ///////MISC
-// var questionTwoE = prompt("What was the name of Atlanta's NHL team?\n\n\nA) Flyers B) Panthers C) Hawks D) Thrashers");
-// if (questionTwoE === "E") {
-//    alert("That Is Correct!");
-// }
-//   else {
-//    alert("Sorry that is incorrect");
-//    }
+// ////// SCIENCE
+$("#button-200E").click( function()
+          {
+var questionTwoE = prompt("This is the most common element on Earth \n\nA) Nitrogen\n\nB) Oxygen \n\nC) Lead \n\nD) Carbon");
+   if (questionTwoE === "B"){
+       alert("That is correct! Please add 200 to your score");
+       }
+       else {
+        alert("That is incorrect. Please subtract 200 from your score");
+         }
+           }
+      );
 
 // ////////////THREE HUNDRED QUESTIONS ///////////////////////////
-// //SPORTS MOVIES MUSIC HISTORY MISC
+// //SPORTS MOVIES MUSIC HISTORY SCIENCE
 // ////////SPORTS
-// var questionThreeA = prompt("What was the name of Atlanta's NHL team?\n\n\nA) Flyers B) Panthers C) Hawks D) Thrashers");
-// if (questionThreeA === "D") {
-//    alert("That Is Correct!");
-// }
-//   else {
-//    alert("Sorry that is incorrect");
-//    }
+$("#button-300A").click( function()
+          {
+var questionThreeA = prompt("The NBA logo is based on this Lakers legend \n\nA) Shaq\n\nB) Magic Johnson \n\nC) Jerry West \n\nD) Kobe Bryant");
+   if (questionThreeA === "C"){
+       alert("That is correct! Please add 300 to your score");
+       }
+       else {
+        alert("That is incorrect. Please subtract 300 from your score");
+         }
+           }
+      );
 // /////////MOVIES
-// var questionThreeB = prompt("What was the name of Atlanta's NHL team?\n\n\nA) Flyers B) Panthers C) Hawks D) Thrashers");
-// if (questionThreeB === "B") {
-//    alert("That Is Correct!");
-// }
-//   else {
-//    alert("Sorry that is incorrect");
-//    }
+$("#button-300B").click( function()
+          {
+var questionThreeB = prompt("I heard your mom goes out with SQUEAK \n\nA) American Pie\n\nB) Beerfest \n\nC) Dogma \n\nD) Baseketball");
+   if (questionThreeB === "D"){
+       alert("That is correct! Please add 300 to your score");
+       }
+       else {
+        alert("That is incorrect. Please subtract 300 from your score");
+         }
+           }
+      );
 // /////////MUSIC
-// var questionThreeC = prompt("What was the name of Atlanta's NHL team?\n\n\nA) Flyers B) Panthers C) Hawks D) Thrashers");
-// if (questionThreeC=== "C") {
-//    alert("That Is Correct!");
-// }
-//   else {
-//    alert("Sorry that is incorrect");
-//    }
+$("#button-300C").click( function()
+          {
+var questionThreeC = prompt("Now I really love how your easy to stack but I really hate how your easy to crack \n\nA) She Thinks My Tractor's Sexy\n\nB) Red Solo Cup \n\nC) Chain Hang Low \n\nD) Roll Out");
+   if (questionThreeC === "B"){
+       alert("That is correct! Please add 300 to your score");
+       }
+       else {
+        alert("That is incorrect. Please subtract 300 from your score");
+         }
+           }
+      );
 // //////HISTORY
-// var questionThreeD = prompt("What was the name of Atlanta's NHL team?\n\n\nA) Flyers B) Panthers C) Hawks D) Thrashers");
-// if (questionThreeD === "D") {
-//    alert("That Is Correct!");
-// }
-//   else {
-//    alert("Sorry that is incorrect");
-//    }
-// ///////MISC
-// var questionThreeE = prompt("What was the name of Atlanta's NHL team?\n\n\nA) Flyers B) Panthers C) Hawks D) Thrashers");
-// if (questionThreeE === "E") {
-//    alert("That Is Correct!");
-// }
-//   else {
-//    alert("Sorry that is incorrect");
-//    }
+$("#button-300D").click( function()
+          {
+var questionThreeD = prompt("This company was the first company to call Ponce City Market its home  \n\nA) Macy's\n\nB) Belk \n\nC) Sears \n\nD) JCPenny");
+   if (questionThreeD === "C"){
+       alert("That is correct! Please add 300 to your score");
+       }
+       else {
+        alert("That is incorrect. Please subtract 300 from your score");
+         }
+           }
+      );
+// ////// SCIENCE
+$("#button-300E").click( function()
+          {
+var questionThreeE = prompt("This was the first element scientifically discovered in 1649  \n\nA) Phosphorous\n\nB) Iron \n\nC) Titanium \n\nD) Magnesium");
+   if (questionThreeE === "A"){
+       alert("That is correct! Please add 300 to your score");
+       }
+       else {
+        alert("That is incorrect. Please subtract 300 from your score");
+         }
+           }
+      );
 
 // ////////////FOUR HUNDRED QUESTIONS ///////////////////////////
-// //SPORTS MOVIES MUSIC HISTORY MISC
+// //SPORTS MOVIES MUSIC HISTORY SCIENCE
 // ////////SPORTS
-// var questionFourA = prompt("What was the name of Atlanta's NHL team?\n\n\nA) Flyers B) Panthers C) Hawks D) Thrashers");
-// if (questionFourA === "D") {
-//    alert("That Is Correct!");
-// }Four
-//   else {
-//    alert("Sorry that is incorrect");
-//    }
+$("#button-400A").click( function()
+          {
+var questionFourA = prompt("To this date, this is the only sport to be played in space  \n\nA) Cheerleading\n\nB) Baseball \n\nC) Basketball \n\nD) Golf");
+   if (questionFourA === "D"){
+       alert("That is correct! Please add 400 to your score");
+       }
+       else {
+        alert("That is incorrect. Please subtract 400 from your score");
+         }
+           }
+      );
 // /////////MOVIES
-// var questionFourB = prompt("What was the name of Atlanta's NHL team?\n\n\nA) Flyers B) Panthers C) Hawks D) Thrashers");
-// if (questionFourB === "B") {
-//    alert("That Is Correct!");
-// }
-//   else {
-//    alert("Sorry that is incorrect");
-//    }
+$("#button-400B").click( function()
+          {
+var questionFourB = prompt("I always tell the truth. Even when I lie.  \n\nA) Remember the Titans\n\nB) The Godfather \n\nC) Scarface \n\nD) James Bond");
+   if (questionFourB === "C"){
+       alert("That is correct! Please add 400 to your score");
+       }
+       else {
+        alert("That is incorrect. Please subtract 400 from your score");
+         }
+           }
+      );
 // /////////MUSIC
-// var questionFourC = prompt("What was the name of Atlanta's NHL team?\n\n\nA) Flyers B) Panthers C) Hawks D) Thrashers");
-// if (questionFourC=== "C") {
-//    alert("That Is Correct!");
-// }
-//   else {
-//    alert("Sorry that is incorrect");
-//    }
+$("#button-400C").click( function()
+          {
+var questionFourC = prompt("Yeah, this album is dedicated to all the teachers that told me I'd never amount to nothin'  \n\nA) Big Poppa\n\nB) Dead Wrong \n\nC) Juicy \n\nD) Mo Money Mo Problems");
+   if (questionFourC === "C"){
+       alert("That is correct! Please add 400 to your score");
+       }
+       else {
+        alert("That is incorrect. Please subtract 400 from your score");
+         }
+           }
+      );
 // //////HISTORY
-// var questionFourD = prompt("What was the name of Atlanta's NHL team?\n\n\nA) Flyers B) Panthers C) Hawks D) Thrashers");
-// if (questionFourD === "D") {
-//    alert("That Is Correct!");
-// }
-//   else {
-//    alert("Sorry that is incorrect");
-//    }
-// ///////MISC
+$("#button-400D").click( function()
+          {
+var questionFourD = prompt("This man was the first president of the US from the state of Georgia  \n\nA) George Bush\n\nB) George Washington \n\nC) Jimmy Carter \n\nD) Donald Trump");
+   if (questionFourD === "C"){
+       alert("That is correct! Please add 400 to your score");
+       }
+       else {
+        alert("That is incorrect. Please subtract 400 from your score");
+         }
+           }
+      );
+//////// SCIENCE
 // var questionFourE = prompt("What was the name of Atlanta's NHL team?\n\n\nA) Flyers B) Panthers C) Hawks D) Thrashers");
 // if (questionFourE === "E") {
 //    alert("That Is Correct!");
@@ -209,7 +249,7 @@ var questionTwoD = prompt("Abraham Lincoln was this number president of the Unit
 //    }
 
 // ////////////FIVE HUNDRED QUESTIONS ///////////////////////////
-// //SPORTS MOVIES MUSIC HISTORY MISC
+// //SPORTS MOVIES MUSIC HISTORY SCIENCE
 // ////////SPORTS
 // var questionFiveA = prompt("What was the name of Atlanta's NHL team?\n\n\nA) Flyers B) Panthers C) Hawks D) Thrashers");
 // if (questionFiveA === "D") {
@@ -242,7 +282,7 @@ var questionTwoD = prompt("Abraham Lincoln was this number president of the Unit
 //   else {
 //    alert("Sorry that is incorrect");
 //    }
-// ///////MISC
+// ////// SCIENCE
 // var questionFiveE = prompt("What was the name of Atlanta's NHL team?\n\n\nA) Flyers B) Panthers C) Hawks D) Thrashers");
 // if (questionFiveE === "E") {
 //    alert("That Is Correct!");
